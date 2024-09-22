d9f52ecc58c5b89edcc6d042adeb6a83ca4249d9
Macbook <aliurinov472@gmail.com>
2024-09-22 13:24:36 +0500

    Oqilov

--- lib/screens/barbers_info/barber_info.dart
+++ lib/screens/barbers_info/barber_info.dart
@@ -1,4 +1,5 @@
 import 'package:alaska_barber/screens/barber_work/barber_project_screen/barber_project_screen.dart';
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:alaska_barber/utils/colors/app_colors.dart';
